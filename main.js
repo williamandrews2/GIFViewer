c1b84d8d76fe3d0b4355cf6913ab78acf9a35c64
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2025-04-20 18:57:06 -0500

    Added reponse status checks.

--- main.js
+++ main.js
@@ -8,22 +8,21 @@ form.addEventListener("submit", (e) => {
 });
 
 function getImg(query) {
-  console.log(query);
   // use the fetch API to make a GET request to the Giphy API
   fetch(
     `https://api.giphy.com/v1/gifs/translate?api_key=5wJBfcqH9p0lxHUOw9opHrWoAHSzb9KR&s=${query}`,
-    { mode: "cors" } // allow cross-origin requests
+    { mode: "cors" }
   )
-    // handle the raw response from the server
     .then(function (response) {
-      // Take the raw Response object, extract and parse the body as JSON,
-      // and pass that parsed JSON data to the next .then() block.
+      if (!response.ok) {
+        alert(`No GIF found for "${query}"!`);
+      }
       return response.json();
     })
-    // handle the JSON data we just parsed
     .then(function (response) {
       img.src = response.data.images.original.url;
-    });
+    })
+    .catch((error) => console.log("ERROR"));
 }
 
 window.onload = getImg("cat");
